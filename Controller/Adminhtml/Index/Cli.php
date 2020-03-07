220d93141c8354f1abcf624f01311498e39e3397
Sipos András <andras.sipos@mzperx.net>
2020-03-07 19:39:47 +0100

    keep log, send email features

--- Controller/Adminhtml/Index/Cli.php
+++ Controller/Adminhtml/Index/Cli.php
@@ -26,6 +26,11 @@ class Cli extends \Magento\Backend\App\Action
     protected $dir;
 
     /**
+     * @var \Magento\Framework\Filesystem\Io\File
+     */
+    protected $file;
+
+    /**
      * Backend auth session
      *
      * @var \Magento\Backend\Model\Auth\Session
@@ -33,24 +38,58 @@ class Cli extends \Magento\Backend\App\Action
     protected $authSession;
 
     /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var string
+     */
+    protected $logFileCommand;
+
+    /**
+     * @var string
+     */
+    protected $logFileResult;
+
+    /**
+     * @var string
+     */
+    protected $configKeepLog;
+
+    /**
+     * @var string
+     */
+    protected $configSendEmail;
+
+    /**
      * Constructor
      *
-     * @param \Magento\Backend\App\Action\Context  $context
-     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
-     * @param \Magento\Framework\Filesystem\DirectoryList $dir
-     * @param \Magento\Backend\Model\Auth\Session $authSession
+     * @param \Magento\Backend\App\Action\Context                $context
+     * @param \Magento\Framework\View\Result\PageFactory         $resultPageFactory
+     * @param \Magento\Framework\Json\Helper\Data                $jsonHelper
+     * @param \Magento\Framework\Filesystem\DirectoryList        $dir
+     * @param \Magento\Backend\Model\Auth\Session                $authSession
+     * @param \Magento\Framework\Filesystem\Io\File              $file
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Framework\Json\Helper\Data $jsonHelper,
         \Magento\Framework\Filesystem\DirectoryList $dir,
-        \Magento\Backend\Model\Auth\Session $authSession
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Framework\Filesystem\Io\File $file,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         $this->resultPageFactory = $resultPageFactory;
         $this->jsonHelper = $jsonHelper;
         $this->dir = $dir;
+        $this->file = $file;
         $this->authSession = $authSession;
+        $this->scopeConfig = $scopeConfig;
+        $this->configKeepLog = true;
+        $this->configSendEmail = true;
         parent::__construct($context);
     }
 
@@ -66,7 +105,7 @@ class Cli extends \Magento\Backend\App\Action
 
             $command = $this->getRequest()->getParam('command');
 
-            $blackCommands = ['admin:user'];
+            $blackCommands = ['admin:user','rm ','sudo'];
             foreach ($blackCommands as $bc) {
                 if (strpos($command, $bc) !== false) {
                     throw new \Exception(__('Error: Cannot run this command due to security reason.'), 1);
@@ -77,13 +116,19 @@ class Cli extends \Magento\Backend\App\Action
                 throw new \Exception(__('cd command is not supported.'), 1);
             }
 
-            $logFile = $this->dir->getPath('var') . '/mfcli.txt';
-            exec($c = 'cd ' . $this->dir->getRoot() . ' && ' . $command . ' > ' . $logFile, $a, $b);
-            $message = file_get_contents($logFile);
+            $this->logFile($command);
+            exec($c = 'cd ' . $this->dir->getRoot() . ' && ' . $command . ' > ' . $this->logFileResult, $a, $b);
+            $message = file_get_contents($this->logFileResult);
             if (!$message) {
                 $message = __('Command not found or error occurred.' . PHP_EOL);
+            }else{
+                $sanitizedCommand = preg_replace('/[^a-z0-9]+/', '-', strtolower($command));
+                $this->logEmail($sanitizedCommand, $message, true);
+                if(!$this->configKeepLog){
+                    unlink($this->logFileCommand);
+                    unlink($this->logFileResult);
+                }
             }
-            unlink($logFile);
         } catch (\Exception $e) {
             $message = $e->getMessage() . PHP_EOL;
         }
@@ -93,6 +138,75 @@ class Cli extends \Magento\Backend\App\Action
         return $this->getResponse()->representJson(
             $this->jsonHelper->jsonEncode($response)
         );
+    }
+
+    /**
+     * @param $command
+     *
+     * @return string
+     */
+    protected function logFile($command)
+    {
+        $this->logFileCommand = '';
+        $this->logFileResult  = '';
+
+        if ( !empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if ( !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        try{
+            $logDir = $this->dir->getPath('var') . '/mfcli';
+            if ( !file_exists($logDir)) {
+                $this->file->mkdir($logDir);
+            }
+            $timestamp         = date('Y-m-d_H:i:s');
+            $sanitizedCommand = preg_replace('/[^a-z0-9]+/', '-', strtolower($command));
+
+            $logFile           = $logDir . '/' . $timestamp . '_' . $sanitizedCommand . '.txt';
+            $logHeader         = 'command: ' . $command . "\n";
+            $logHeader         .= 'sanitized command: ' . $sanitizedCommand . "\n";
+            $logHeader         .= 'administrator: ' . $this->authSession->getUser()->getUsername() . ' / ' . $this->authSession->getUser()->getEmail() . "\n";
+            $logHeader         .= 'timestamp: ' . $timestamp . "\n" ;
+            $logHeader         .= 'ip address: ' . $ip . "\n";
+
+            file_put_contents($logFile, $logHeader);
+            $this->logEmail($sanitizedCommand,$logHeader);
+        }catch (\Exception $e) {
+            return false;
+        }
+
+        $this->logFileCommand = $logFile;
+        $this->logFileResult  = $logDir . '/' . $timestamp . '_' . $sanitizedCommand . '_result.txt';
+
+        return true;
+    }
+
+    /**
+     * @param      $subject
+     * @param      $body
+     * @param bool $result
+     *
+     * @return bool
+     */
+    protected function logEmail($subject, $body, $result = false)
+    {
+        try {
+            if ($this->configSendEmail) {
+                $subjectType = ($result ? 'result' : 'command');
+                $email       = new \Zend_Mail();
+                $email->setSubject("Magento 2 cli $subjectType: " . $subject);
+                $email->setBodyText($body);
+                $email->addTo($this->scopeConfig->getValue('trans_email/ident_custom2/email'), $this->scopeConfig->getValue('trans_email/ident_custom2/name'));
+                $email->send();
+            }
+            return true;
+        } catch (\Zend_Mail_Exception $e) {
+            return false;
+        }
     }
 
         /**
